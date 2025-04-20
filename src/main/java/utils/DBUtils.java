85562de1506f0e5e3597089e3d9a8cde28176d48
Rohan Poudel <>
2025-04-20 13:09:25 +0545

    working in utils

--- src/main/java/utils/DBUtils.java
+++ src/main/java/utils/DBUtils.java
@@ -1,4 +1,14 @@
 package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtils {
+    private static final String URL = "jdbc:mysql://localhost:3306/aayojana";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
 }
