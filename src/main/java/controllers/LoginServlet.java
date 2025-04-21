c1deec388acfb65533cf8e9d09f36a273c109b65
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-04-21 22:18:33 +0545

    Updating DAO properly for USER login

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,4 +1,23 @@
 package controllers;
 
-public class LoginServlet {
+import dao.UserDAO;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO dao = new UserDAO();
+
+        boolean isValid = dao.loginUser(email,password);
+        if(isValid){
+            HttpSession session =
+        }
+
+
+    }
 }
