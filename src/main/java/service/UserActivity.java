5d27535dce21054146dea697c2966d3583b1856f
Rohan Poudel <np05cp4s240029@iic.edu.np>
2025-04-25 22:35:31 +0545

    User activity

--- src/main/java/service/UserActivity.java
+++ src/main/java/service/UserActivity.java
@@ -1,11 +1,11 @@
-//package service;
-//
-//import dao.UserActivityDAO;
-//
-//public class UserActivity {
-//    public boolean updateDetails(String name, String email, String phone, String photo, int id){
-//        UserActivityDAO activity = new UserActivityDAO();
-//
-//
-//    }
-//}
+package service;
+
+import dao.UserActivityDAO;
+
+public class UserActivity {
+    public boolean updateDetails(String name, String email, String phone, String photo, int id){
+        UserActivityDAO activity = new UserActivityDAO();
+
+
+    }
+}
